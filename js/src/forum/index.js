eaf3077042a5b46a56fd92a29dd79b9132b4a260
samerton <samerton@users.noreply.github.com>
2021-04-07 21:35:53 +0100

    Add colour to profile page usernames too

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,44 +1,63 @@
 import app from 'flarum/app';
-import {extend} from 'flarum/extend';
+import { extend } from 'flarum/extend';
 import PostUser from 'flarum/components/PostUser';
+import UserCard from 'flarum/components/UserCard';
 import Link from 'flarum/components/Link';
 
+function matchTag(tag) {
+  return node => node && node.tag && node.tag === tag;
+}
+
+function matchClass(className) {
+  // trim() to handle classNames that end with spaces easier
+  return node => node && node.attrs && node.attrs.className && node.attrs.className.trim() === className;
+}
+
+function applyColor(vdom, user) {
+  // Find the first group that has a color
+  // We don't read badges because we would need to support every badge component and its attrs
+  const firstColoredGroup = user.groups().find(group => {
+    return group.color();
+  });
+
+  // If there are no color groups, skip
+  if (!firstColoredGroup) {
+    console.log('No colour');
+    return;
+  }
+
+  console.log(vdom);
+
+  console.log(firstColoredGroup.color());
+
+  vdom.attrs = vdom.attrs || {};
+  vdom.attrs.style = vdom.attrs.style || {};
+  vdom.attrs.style.color = firstColoredGroup.color();
+}
+
 app.initializers.add('mickmelon-coloured-usernames', () => {
-    extend(PostUser.prototype, 'view', function (vnode) {
-        const user = this.attrs.post.user();
+  extend(PostUser.prototype, 'view', function (vnode) {
+    const user = this.attrs.post.user();
 
-        // If the post belongs to a deleted user, skip
-        if (!user) {
-            return;
-        }
+    // If the post belongs to a deleted user, skip
+    if (!user) {
+      return;
+    }
 
-        // Find the first group that has a color
-        // We don't read badges because we would need to support every badge component and its attrs
-        const firstColoredGroup = user.groups().find(group => {
-          return group.color();
-        });
+    const avatar = vnode.children.find(matchTag('h3'))
+      .children.find(matchTag(Link))
+      .children.find(matchClass('username'));
 
-        // If there are no color groups, skip
-        if (!firstColoredGroup) {
-            return;
-        }
+    applyColor(avatar, user);
+  });
 
-        const matchTag = tag => {
-            return node => node && node.tag && node.tag === tag;
-        };
+  extend(UserCard.prototype, 'view', function (vnode) {
+    const username = vnode.children.find(matchClass('darkenBackground'))
+      .children.find(matchClass('container'))
+      .children.find(matchClass('UserCard-profile'))
+      .children.find(matchClass('UserCard-identity'))
+      .children.find(matchClass('username'));
 
-        const matchClass = className => {
-            return node => node && node.attrs && node.attrs.className && node.attrs.className === className;
-        };
-
-        // Set the colour of the username class
-        // This is the only part that has been changed from clark's repo :)
-        const avatar = vnode.children.find(matchTag('h3'))
-            .children.find(matchTag(Link))
-            .children.find(matchClass('username'));
-
-        avatar.attrs = avatar.attrs || {};
-        avatar.attrs.style = avatar.attrs.style || {};
-        avatar.attrs.style.color = firstColoredGroup.color();
-    });
+      applyColor(username, this.attrs.user);
+  });
 });
